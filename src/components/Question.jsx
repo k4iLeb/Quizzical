032a0c6f170f3771bd92efaca904d3e283bfdd6c
k4iLeb <k4ilebi@gmail.com>
2023-02-13 00:46:56 +0200

    mapped answers, added pointer

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,17 +1,26 @@
+import { nanoid } from "nanoid";
 import React from "react";
-import { convertUnicode } from "../util";
 
 function Question(props) {
-  // console.log(props);
-  // const question = convertUnicode(props.q.question);
+  console.log(props.q.all_answers);
+
+  const answersElement = props.q.all_answers.map((x) => {
+    return (
+      <button key={nanoid()} answer={x} id={null} className="btn-answers">
+        {x}
+      </button>
+    );
+  });
+
   return (
     <div className="question">
       <h4 className="quiz--question">{props.q.question}</h4>
       <div className="quiz--answers">
-        <button className="btn-answers">asnwer 1</button>
+        {/* <button className="btn-answers">asnwer 1</button>
         <button className="btn-answers">asnwer 2</button>
         <button className="btn-answers">asnwer 3</button>
-        <button className="btn-answers">asnwer 4</button>
+        <button className="btn-answers">asnwer 4</button> */}
+        {answersElement}
       </div>
       <hr />
     </div>
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,9 @@
 import { nanoid } from "nanoid";
 import React, { useEffect, useState } from "react";
-import { convertUnicode } from "../util";
 import Question from "./Question";
 
 export default function Quiz() {
   const [quizQs, setQuizQs] = useState([]);
-  // console.log(qs);
-
-  // useEffect(() => {
-  //   // console.log("Effect ran");
-  //   fetch(
-  //     "https://opentdb.com/api.php?amount=5&category=18&type=multiple&encode=base64"
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => setApiQs(data.results));
-  // }, []);
 
   // shuffle array
   const shuffledArray = (arr) => arr.sort(() => Math.random() - 0.5);
@@ -39,33 +28,10 @@ export default function Quiz() {
         });
       });
       setQuizQs(q);
-      console.log(quizQs);
-      // setAllMemes(data.data.memes);
+      // console.log(quizQs);
     }
     getQuestions();
   }, []);
-
-  // useEffect(() => {
-  //   var questionsArray = [...apiQs];
-  //   questionsArray.forEach((x) => delete x.category);
-  //   questionsArray.forEach((x) => delete x.type);
-  //   questionsArray.forEach((x) => delete x.difficulty);
-  //   // added id (to resolve key error)
-  //   let count = 1;
-  //   questionsArray = questionsArray.map((x) => {
-  //     return { ...x, id: count++ };
-  //   });
-  //   questionsArray = questionsArray.map((x) => {
-  //     return {
-  //       ...x,
-  //       all_answers: x.incorrect_answers.concat(x.correct_answer),
-  //     };
-  //   });
-
-  // console.log(questionsArray);
-  //   setQuizQs([...questionsArray]);
-  //   console.log(quizQs);
-  // }, []);
 
   const question = quizQs.map((q) => <Question key={q.id} q={q} />);
 
